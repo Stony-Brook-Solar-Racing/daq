22c7403f5dbd2083b6e9b2c5824269ff7af77ed7
Daniel Lew <lewdaniel2@gmail.com>
2025-02-28 17:22:54 -0500

    initial setup started

--- setup.py
+++ setup.py
@@ -7,6 +7,12 @@ from Volatage_reader import VoltageReader
 # Constants
 BAUDRATE = 9600
 
+logging.basicConfig(
+    filename="/var/log/daq/daq.log",
+    filemode='w+',
+    level=logging.INFO
+)
+
 class Setup:
     def __setup_arduino(self):
         if sys.platform == "linux":
@@ -16,8 +22,18 @@ class Setup:
         else:
            raise Exception("Platform not supported")
 
-    for port in ports:
-        try:
-            self.arduino = Serial(port=port, baudrate=BAUDRATE, timeout=TIMEOUT)
-        except(OSError, SerialException):
-            return 0;
+        for port in ports:
+            try:
+                self.arduino = Serial(port=port, baudrate=BAUDRATE, timeout=TIMEOUT)
+            except(OSError, SerialException) as err:
+                logging.error(err)
+        
+        if self.arduino == None:
+
+
+    def __init__(self):
+
+        
+
+
+
