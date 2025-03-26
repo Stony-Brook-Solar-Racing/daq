9baf026f01d90f4485c1ea7c4bbead13cde49cc4
bzhang921 <bzhang575@gmail.com>
2025-03-26 17:43:46 -0400

    Reworked database schemas

--- database.py
+++ database.py
@@ -1,42 +1,81 @@
 from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import Session
 from datetime import datetime
-from models import Data
+from models import Measurements, Runs
 
 class Database():
+
+    id: int
     
     def __init__(self):
         
         self.engine = create_engine("sqlite:///db/test.db", echo=True, pool_pre_ping=True)
+        inspect_obj = inspect(self.engine)
         
-        # if table does not exist
-        if not inspect(self.engine).has_table(Data.__tablename__):
-            Data.metadata.create_all(bind=self.engine)
+        # create tables if they don't exist yet
+        if not inspect_obj.has_table(Measurements.__tablename__):
+            Measurements.metadata.create_all(bind=self.engine)
+        
+        if not inspect_obj.has_table(Runs.__tablename__):
+            Runs.metadata.create_all(bind=self.engine)
+
+        # makes sure that the table reflects the schema for now
+        # may want to delete this later
+        # Measurements.metadata.reflect(self.engine, Measurements)
+        # Runs.metadata.reflect(self.engine, Runs)
+
+        # creates a new run on launch
+        run = self.create_run()
+        if run != None:
+            self.id = run.id
+        else:
+            raise Exception('Unable to create new run in database')
+        print(run)
 
     # decorator for interacting with database
     def use_session(func):
         def wrap(self, *args, **kwargs):
-            with Session(self.engine) as session:
-                func(self, session, *args, **kwargs)
+            # expire_on_commit set to True for now to grab some info from the created
+            # row in Runs
+            # otherwise the objects added and returned from queries will expire
+            # and can no longer be access
+            with Session(self.engine, expire_on_commit=False) as session:
+                return func(self, session, *args, **kwargs)
         return wrap
+
+    @use_session
+    def create_run(self, session: Session):
+        try:
+            run = Runs( # inserts test data for now
+                boat_max_v=38,
+                boat_min_v=34
+            )
+            session.add(run)
+            session.commit()
+            return run
+        except Exception as e:
+            print(e)
+            session.rollback()
     
     @use_session
-    def add_voltage(self, session: Session, pre_shunt, post_shunt):
-        session.begin()
+    def add_voltage(self, session: Session, pre_shunt, post_shunt, current=-1):
         current_time = datetime.now()
         try:
-            session.add(Data(
+            measurement = Measurements(
                 pre_shunt=pre_shunt,
                 post_shunt=post_shunt,
-                current=-1,
-                timestamp=current_time
-            ))
+                current=current,
+                timestamp=current_time,
+                run_id=self.id
+            )
+            session.add(measurement)
             session.commit()
+            return measurement
         except Exception as e:
             print(e)
             session.rollback()
 
 # tests if db works
-# if __name__ == '__main__':
-#     db = Database()
-#     db.add_voltage(pre_shunt=1, post_shunt=1)
+if __name__ == '__main__':
+    db = Database()
+    db.add_voltage(pre_shunt=1, post_shunt=1, current=-1)
--- models.py
+++ models.py
@@ -1,29 +1,41 @@
-from sqlalchemy import Float
+from sqlalchemy import Float, Integer, ForeignKey
+from sqlalchemy.sql import func
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import mapped_column, foreign, relationship
 from sqlalchemy.orm import Mapped
+from typing import List
+from sqlalchemy.schema import Index 
 from sqlalchemy import DateTime
 from datetime import datetime
 
 class Base(DeclarativeBase):
     pass
 
-class Data(Base):
-
-    __tablename__ = 'data'
-    id: Mapped[int] = mapped_column(primary_key=True)
+# Table of key measurements:
+class Measurements(Base):
+    __tablename__ = 'measurements'
+    measurement_id: Mapped[int] = mapped_column(Integer(), primary_key=True, autoincrement=True)
+    run_id: Mapped[int] = mapped_column(Integer(), ForeignKey('runs.id'), index=True, nullable=False)
     pre_shunt: Mapped[float] = mapped_column(Float(2))
     post_shunt: Mapped[float] = mapped_column(Float(2))
     current: Mapped[float] = mapped_column(Float(2))
-    timestamp: Mapped[datetime] = mapped_column(DateTime())
+    run: Mapped["Runs"] = relationship(back_populates="measurements")
+    timestamp: Mapped[datetime] = mapped_column(DateTime(), default=func.now())
+    # __table_args__ = (Index('measurement_run_id', 'run.id'))
 
     def __repr__(self):
-        return f"Voltage Record: {self.pre_shunt}"
+        return f"""timestamp: {self.timestamp}
+pre_shunt: {self.pre_shunt}
+post_shunt: {self.post_shunt}
+current: {self.current}"""
 
-# class MeasurementSpecs(Base):
-#     __tablename__ = 'measurement_specs'
-
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     boat_max_v = Mapped[float] = mapped_column(Float(2))
-#     boat_min_v = Mapped[float] = mapped_column(Float(2))
+# Table that holds any constants or other information about the run
+class Runs(Base):
+    __tablename__ = 'runs'
+    id: Mapped[int] = mapped_column(Integer(), primary_key=True, autoincrement=True)
+    boat_max_v: Mapped[float] = mapped_column(Float(2))
+    boat_min_v: Mapped[float] = mapped_column(Float(2))
+    # This relationship with measurments should cascade delete all measurments
+    # with an associated run_id
+    measurements: Mapped[List["Measurements"]] = relationship(back_populates="run", cascade="all, delete")
 
