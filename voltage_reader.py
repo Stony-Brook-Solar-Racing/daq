9fce8a8c9a08870b524e1d238fbb8edaa13b1697
nuuenkdaniel <lewdaniel2@gmail.com>
2025-04-12 12:39:10 -0400

    Updated calculations for voltage

--- voltage_reader.py
+++ voltage_reader.py
@@ -23,23 +23,21 @@ class VoltageReader:
             data = arduino.readline()
             voltage = data.decode().strip().split()
             if len(voltage) > 0:
-                # print(f"v1: {voltage[0]}, v2: {voltage[1]}") # , diff: {voltage[2]}\n") 
-                
+                print(f"v1: {voltage[0]}, v2: {voltage[1]}") # , diff: {voltage[2]}\n") 
                 db.add_voltage(pre_shunt=float(voltage[0]), post_shunt=float(voltage[1]))
                 ttd = self.calc_ttd(0, 0);
                 battery = self.calc_battery(0, 34, 38)
                 print(f"ttd: {ttd}, battery: {battery}")
                 redis.set_value("ttd", ttd)
-                redis.set_value("battery", 85)
+                redis.set_value("battery", battery)
 
         print("Exitted")
 
-    def calc_ttd(self, v, curr) -> float:
-        return 2.0
+    def calc_ttd(self, capacity, curr) -> float:
+        return capactiy/curr
 
     def calc_current(self, v1, v3, res) -> float:
-        # (v3-v1)/res
-        return 0
+        return ((v3-v1)/res)*1000
 
     def calc_battery(self, v, min_v, max_v) -> int:
         return int((max_v-v)/(max_v-min_v))
